36c03fd994d6ca7dfcd6f4858bee5fe03e1d381e
Luiz R. <luizroberto44630@gmail.com>
2024-08-21 14:12:00 -0300

    Dinamic Routes p1

--- app/routers/router.php
+++ app/routers/router.php
@@ -4,16 +4,29 @@ function routers() {
 }
 function existsRoute($uri, $routes) {
     if (array_key_exists($uri, $routes)) {
-        return 'achou';
+        return [$uri => $routes[$uri]];
     }
-    return 'n achou';
+    return [];
+}
+
+function regexRoute($uri, $routes) {
+    return array_filter(
+        // array_keys($routes) func para pegar somente as keys do array
+        $routes, 
+        function($value) use($uri){
+            $regex = str_replace('/', '\/', ltrim($value, '/'));
+            return preg_match("/^$regex$/", ltrim($uri, '/'));
+        },
+        ARRAY_FILTER_USE_KEY // Flag opcional dizendo que é para pegar semente as keys
+    );
 }
 function getRoute() {
     $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
     $routes = routers();
-
     $existsRoute =  existsRoute($uri, $routes);
-
-    return $existsRoute;
+    if (empty($existsRoute)){
+        $existsRoute = regexRoute($uri, $routes);
+    }
+    var_dump($existsRoute);  
 }
 ?>
--- app/routers/uriRouters.php
+++ app/routers/uriRouters.php
@@ -1,6 +1,7 @@
 <?php
 return [
         '/' => 'Home@index',
-        '/user/create' => 'User@create'
+        '/user/create' => 'User@create',
+        '/user/[a-z0-9]+' => 'User@index'
     ];
 ?>
